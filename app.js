28fccfa6a3055196422c06c542ead53615df9846
Matthew Wang <matwan@bergen.org>
2022-05-12 15:12:23 -0400

    code

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ const port = 8080;
 // Configure Express to use EJS
 app.set( "views",  __dirname + "/views");
 app.set( "view engine", "ejs" );
+// // Configure Express to parse incoming JSON data
+// app.use( express.json() );
+// Configure Express to parse URL-encoded POST request bodies (traditional forms)
+app.use( express.urlencoded({ extended: false }) );
 
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
+
+// define middleware that serves static resources in the public directory
+app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
@@ -35,7 +42,7 @@ app.get( "/stuff", ( req, res ) => {
 } );
 
 // define a route for the item detail page
-const read_item_sql = `
+const read_stuff_item_sql = `
     SELECT 
         id, item, quantity, description 
     FROM
@@ -44,7 +51,7 @@ const read_item_sql = `
         id = ?
 `
 app.get( "/stuff/item/:id", ( req, res ) => {
-    db.execute(read_item_sql, [req.params.id], (error, results) => {
+    db.execute(read_stuff_item_sql, [req.params.id], (error, results) => {
         if (error)
             res.status(500).send(error); //Internal Server Error
         else if (results.length == 0)
@@ -57,6 +64,63 @@ app.get( "/stuff/item/:id", ( req, res ) => {
         }
     });
 });
+
+// define a route for item DELETE
+const delete_item_sql = `
+    DELETE 
+    FROM
+        stuff
+    WHERE
+        id = ?
+`
+app.get("/stuff/item/:id/delete", ( req, res ) => {
+    db.execute(delete_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect("/stuff");
+        }
+    });
+})
+
+// define a route for item UPDATE
+const update_item_sql = `
+    UPDATE
+        stuff
+    SET
+        item = ?,
+        quantity = ?,
+        description = ?
+    WHERE
+        id = ?
+`
+app.post("/stuff/item/:id", ( req, res ) => {
+    db.execute(update_item_sql, [req.body.name, req.body.quantity, req.body.description, req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/stuff/item/${req.params.id}`);
+        }
+    });
+})
+
+// define a route for item CREATE
+const create_item_sql = `
+    INSERT INTO stuff
+        (item, quantity)
+    VALUES
+        (?, ?)
+`
+app.post("/stuff", ( req, res ) => {
+    db.execute(create_item_sql, [req.body.name, req.body.quantity], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            //results.insertId has the primary key (id) of the newly inserted element.
+            res.redirect(`/stuff/item/${results.insertId}`);
+        }
+    });
+})
 
 // start the server
 app.listen( port, () => {
